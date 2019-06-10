6b9679e711ff3cd7ec5d840725a0f6002573b145
Cody Luth <luthcody@gmail.com>
2019-06-10 01:26:26 -0700

    Deleted Photos & Merged Main

--- api/index.js
+++ api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 
 router.use('/courses', require('./courses'));
-router.use('/businesses', require('./businesses'));
-router.use('/photos', require('./photos'));
 router.use('/assignments', require('./assignments'));
 router.use('/users', require('./users'));
 
